71e9539642e0eb61362a2a56ddffa5c39c77ed45
Cellardoor <cellard00r@ukr.net>
2022-05-10 00:05:32 +0300

    10.05.2022

--- project1.js
+++ project1.js
@@ -1,15 +1,41 @@
 const numberOfFilms = +prompt("How much films have you seen", "0");
 //const numberOfFilms = 5;
-const personalMoviDb = {
+const personalMovieDb = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
-console.log(personalMoviDb);
-let filmName = prompt("One of the last films you seen", ""),
-  score = prompt("Mark it", "");
+console.log(personalMovieDb);
 
-personalMoviDb.movies[filmName] = score;
-console.log(personalMoviDb);
+for (let i = 0; i < 2; i++) {
+  const filmName = prompt("One of the last films you seen", ""),
+    score = +prompt("Mark it", "");
+
+  //console.log(personalMovieDb);
+  if (
+    filmName != null &&
+    score != null &&
+    filmName.length < 50 &&
+    filmName != "" &&
+    score != ""
+  ) {
+    personalMovieDb.movies[filmName] = score;
+
+    console.log("everything is good");
+  } else {
+    console.log("You are input something wrong");
+    i--;
+  }
+  //  console.log(filmName.length);
+}
+if (personalMovieDb.count < 10) {
+  console.log("You have seen a few films");
+} else if (personalMovieDb >= 10 && personalMovieDb.count < 30) {
+  console.log("Youre normal guy");
+} else if (personalMovieDb.count >= 30) {
+  console.log("You have seen a lot of films");
+} else {
+  console.log("Error");
+}
