f17acf0dd37a7c9048ceba1628ddf2af172ab0be
Cellardoor <cellard00r@ukr.net>
2022-05-12 00:32:44 +0300

    12.05.2022

--- project1.js
+++ project1.js
@@ -1,5 +1,11 @@
-const numberOfFilms = +prompt("How much films have you seen", "0");
-//const numberOfFilms = 5;
+let numberOfFilms;
+function start() {
+  numberOfFilms = +prompt("How much films have you seen", "0");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("How much films have you seen", "0");
+  }
+}
+start();
 const personalMovieDb = {
   count: numberOfFilms,
   movies: {},
@@ -9,33 +15,55 @@ const personalMovieDb = {
 };
 console.log(personalMovieDb);
 
-for (let i = 0; i < 2; i++) {
-  const filmName = prompt("One of the last films you seen", ""),
-    score = +prompt("Mark it", "");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const filmName = prompt("One of the last films you seen", ""),
+      score = +prompt("Mark it", "");
 
-  //console.log(personalMovieDb);
-  if (
-    filmName != null &&
-    score != null &&
-    filmName.length < 50 &&
-    filmName != "" &&
-    score != ""
-  ) {
-    personalMovieDb.movies[filmName] = score;
+    //console.log(personalMovieDb);
+    if (
+      filmName != null &&
+      score != null &&
+      filmName.length < 50 &&
+      filmName != "" &&
+      score != ""
+    ) {
+      personalMovieDb.movies[filmName] = score;
 
-    console.log("everything is good");
-  } else {
-    console.log("You are input something wrong");
-    i--;
+      console.log("everything is good");
+    } else {
+      console.log("You are input something wrong");
+      i--;
+    }
   }
-  //  console.log(filmName.length);
 }
-if (personalMovieDb.count < 10) {
-  console.log("You have seen a few films");
-} else if (personalMovieDb >= 10 && personalMovieDb.count < 30) {
-  console.log("Youre normal guy");
-} else if (personalMovieDb.count >= 30) {
-  console.log("You have seen a lot of films");
-} else {
-  console.log("Error");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDb.count < 10) {
+    console.log("You have seen a few films");
+  } else if (personalMovieDb >= 10 && personalMovieDb.count < 30) {
+    console.log("Youre normal guy");
+  } else if (personalMovieDb.count >= 30) {
+    console.log("You have seen a lot of films");
+  } else {
+    console.log("Error");
+  }
 }
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDb);
+  }
+  showMyDB(personalMovieDb.privat);
+}
+const writeYourGenres = function () {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDb.genres[i - 1] = prompt(
+      `Input your favorite genre in order ${i}`
+    );
+  }
+};
+writeYourGenres();
+console.dir(personalMovieDb.genres);
